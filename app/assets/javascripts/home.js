4758be910ee1de1c4fbb6a7bb5d98ee271a07a1f
Paul Yun <paul.a.yun@gmail.com>
2015-10-26 18:38:31 -0400

    WIP.

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,24 @@
+// console.log("Hello World");
+
+// var a = 1;
+// var b = 2;
+//
+// var c = a + b;
+// console.log("The sum of " + a + " and " + b + " is " + c);
+
+// var numbers = [3,2,6];
+// for (var i = 0; i < numbers.length; i++){
+//   console.log(numbers[i]);
+// }
+
+var number = 1
+
+function numberCategorizer(number) {
+  if (number = 1){
+    return "integer"
+  };
+
+};
+
+var test = numberCategorizer(number);
+console.log(number + " is a " + test)
